5e9ed79ba91b027aa0df88949b9c9d52457264a3
NicolasPanda <91343504+NicolasPanda@users.noreply.github.com>
2022-04-22 15:04:12 -0400

    player shoot

--- Assets/Character/PlayerController.cs
+++ Assets/Character/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.InputSystem;
@@ -12,17 +13,29 @@ public class PlayerController : MonoBehaviour
     private CharacterController controller;
     [SerializeField]
     private float gravityValue = -9.81f;
-    
+    [SerializeField]
+    private float bulletSpeed = 10f;
     private Vector3 _playerVelocity;
+
+    [SerializeField]
+    private GameObject bullet;
+    
+    private PlayerInput _playerInput;
+    private InputAction _shootInput;
+    
+    
     public PlayerType playerType;
 
     private Vector2 _movementInput = Vector2.zero;
     private Vector2 _rotateInput = Vector2.zero;
-    private bool _shootInput = false;
 
     private void Start()
     {
+        _playerInput = gameObject.GetComponent<PlayerInput>();
         controller = gameObject.GetComponent<CharacterController>();
+        
+        _shootInput = _playerInput.currentActionMap.FindAction("Shoot");
+        _shootInput.started += ctx => Fire();
     }
 
     public void onMove(InputAction.CallbackContext context)
@@ -30,11 +43,6 @@ public class PlayerController : MonoBehaviour
         _movementInput = context.ReadValue<Vector2>();
     }
 
-    public void onShoot(InputAction.CallbackContext context)
-    {
-        _shootInput = context.action.triggered;
-    }
-    
     public void onRotate(InputAction.CallbackContext context)
     {
         _rotateInput = context.ReadValue<Vector2>();
@@ -50,9 +58,15 @@ public class PlayerController : MonoBehaviour
         {
             gameObject.transform.forward = rotate;
         }
-        
+
         controller.Move(_playerVelocity * Time.deltaTime);
         _playerVelocity.y += gravityValue * Time.deltaTime;
+    }
+
+    void Fire()
+    {
+        var newBullet = Instantiate(bullet, transform.position, transform.rotation);
+        newBullet.GetComponent<Rigidbody>().velocity = -transform.forward * bulletSpeed;
     }
 }
 
